6f2c412a50e86a2aa484cae564a216606698ae00
annamuc <annamaria.mucciante@gmail.com>
2019-03-06 10:58:35 +0100

    Ciao povery

---
+++ pugbet/src/main/java/com/scommesse/pugbet/PugbetApplication.java
@@ -0,0 +1,13 @@
+package com.scommesse.pugbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PugbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PugbetApplication.class, args);
+	}
+
+}
