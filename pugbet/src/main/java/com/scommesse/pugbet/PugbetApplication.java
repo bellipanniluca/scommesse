0780bc366697018422ffe621da2f753052516337
GianmarcoMar <marangon.gianmarco@gmail.com>
2019-03-06 11:31:33 +0100

    add branch

--- pugbet/src/main/java/com/scommesse/pugbet/PugbetApplication.java
+++ pugbet/src/main/java/com/scommesse/pugbet/PugbetApplication.java
@@ -1,10 +1,11 @@
-package com.scommesse.pugbet;
+ùpackage com.scommesse.pugbet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PugbetApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PugbetApplication.class, args);
