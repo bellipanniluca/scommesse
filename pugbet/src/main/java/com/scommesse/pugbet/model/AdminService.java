b467bcd83e4e23e7f7bf3127682877b021e93e61
annamuc <annamaria.mucciante@gmail.com>
2019-03-14 09:24:55 +0100

    admin classe, interfaccia e pagina (aggiornare anche il mycontroller per reindirizzare la pagina)

--- pugbet/src/main/java/com/scommesse/pugbet/model/AdminService.java
+++ pugbet/src/main/java/com/scommesse/pugbet/model/AdminService.java
@@ -1,13 +1,24 @@
 package com.scommesse.pugbet.model;
 
-public interface AdminService {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.scommesse.pugbet.controller.PartitaBasket;
+import com.scommesse.pugbet.controller.PartitaCalcio;
+import com.scommesse.pugbet.controller.PartitaHockey;
+import com.scommesse.pugbet.controller.SchedinaFinale;
+import com.scommesse.pugbet.controller.Utente;
+
+public interface AdminService extends JpaRepository<Admin, Integer>{
 	
-	void deleteUtente(Utente utente);
-	void deletePartitaCalcio(PartitaCalcio partitaCalcio);
-	void deletePartitaBasket(PartitaBasket partitaBasket);
-	void deletePartitaHockey(PartitaHockey partitaHockey);
+	void deleteById(Utente utente); //elimina utente dal db
+	void deleteById(SchedinaFinale schedinaFinale); //elimina schedina dal db (da usare insieme ad utente)
+	void deleteById(PartitaCalcio partitaCalcio); 
+	void deleteById(PartitaBasket partitaBasket);
+	void deleteById(PartitaHockey partitaHockey);
+	
 	void addPartitaCalcio(PartitaCalcio partitaCalcio);
 	void addPartitaBasket(PartitaBasket partitaBasket);
 	void addPartitaHockey(PartitaHockey partitaHockey);
+	
 	
 }
