c88cb9211549afb6b37b3606bc343b8db1a2b20c
GianmarcoMar <47633016+GianmarcoMar@users.noreply.github.com>
2019-03-21 10:48:33 +0100

    Update SchedinaFinaleService.java
    cancella le schedina associate all'utente

--- pugbet/src/main/java/com/scommesse/pugbet/controller/SchedinaFinaleService.java
+++ pugbet/src/main/java/com/scommesse/pugbet/controller/SchedinaFinaleService.java
@@ -3,9 +3,17 @@ package com.scommesse.pugbet.controller;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface SchedinaFinaleService extends JpaRepository<SchedinaFinale, Integer> {
 	
 	public List<SchedinaFinale> findByIdUtente(int id);
+	
+	@Modifying
+	@Transactional
+	public void deleteByIdUtente(int id);
 
 }
